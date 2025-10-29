a2648da2371c2f4896757574ad1cf39e82db5a40
daniiel211 <ignacio.vergaracaroca@gmail.com>
2025-10-29 14:57:53 -0300

    serializer

---
+++ serial_app/urls.py
@@ -0,0 +1,8 @@
+from serial_app import views
+from django.urls import path
+
+urlpatterns = [
+    path('students/', views.student_list),
+    path('students/<int:pk>/', views.student_detail),
+]
+    
