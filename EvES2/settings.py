44fc7218a0fee9e519e8aa3ab04d10db2f421dea
daniiel211 <ignacio.vergaracaroca@gmail.com>
2025-11-26 14:47:18 -0300

    railway host

--- EvES2/settings.py
+++ EvES2/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS','impulsa-chile-production-ae96.up.railway.app', cast=Csv())
 #SECURE_SSL_REDIRECT = True
 
     
